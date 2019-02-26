3e2073ae0545133304924c4d8ef6f0e0919dc884
Rakesh Maharjan <rakesh.maharjan1991@gmail.com>
2019-02-26 10:52:34 +1000

    Simple Calculator

---
+++ SimpleCalculator/SimpleCalculator.Test.Unit/CalculatorEngineTest.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class CalculatorEngineTest
+
+    { 
+
+        private readonly CalculatorEngine _calculatorEngine = new CalculatorEngine();
+        [TestMethod]
+        public void AddsTwoNumbersAndReturnsValidResultsForNonSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("add", number1, number2);
+            Assert.AreEqual(3, result);
+        }
+
+        public void AddsTwoNumbersAndReturnsValidResultsForSymbolOperation()
+        {
+            int number1 = 1;
+            int number2 = 2;
+            double result = _calculatorEngine.Calculate("+", number1, number2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ SimpleCalculator/SimpleCalculator/InputConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber)) throw new ArgumentException("Expected Numeric Value.");
+            return convertedNumber;
+        }
+    }
+}
